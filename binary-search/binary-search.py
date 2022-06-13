9ab506517151265bd140314dcd373736d3d2edbb
jinyeong Kim <56948757+jinyeong0142@users.noreply.github.com>
2022-06-13 16:04:30 +0900

    Time: 448 ms, Memory: 15.4 MB - LeetHub

---
+++ binary-search/binary-search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+        
+        answer = -1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                answer = mid
+                break
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return answer
